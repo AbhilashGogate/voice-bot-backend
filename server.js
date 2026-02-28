22abc556a1a1d1bf9b024f3441be1ce92d4362a3
Abhilash Gogate <abhilashgogate51@gmail.com>
2026-02-28 14:07:57 +0530

    fix cors correctly

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
+    credentials: true
     
   })
 );
