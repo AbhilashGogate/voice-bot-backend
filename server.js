0bdb23d134ea6178e5f90481c23c07f7b47913d8
Abhilash Gogate <abhilashgogate51@gmail.com>
2026-02-28 14:23:57 +0530

    manual CORS header fix

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ dotenv.config();
 
 const app = express();
 
-// ✅ CORS FIRST
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST"],
-  
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 
 
