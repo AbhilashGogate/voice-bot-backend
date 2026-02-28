068ad6b37cb1aa606df72d1a2a0886c7851bf300
Abhilash Gogate <abhilashgogate51@gmail.com>
2026-02-28 14:04:35 +0530

    final port fix for render

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    methods: ["GET", "POST"],
+    
   })
 );
  // Handle preflight requests
